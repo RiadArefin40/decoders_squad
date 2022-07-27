14fb86c4a5a417db6076ca8383bb28b44231dfbf
RiadArefin <arefinriad40@gmail.com>
2022-07-27 19:36:10 +0600

    banner added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -12,23 +12,31 @@ const Banner = () => {
      speed: 1000,
      autoplaySpeed: 2000,
      cssEase: "linear",
+
    
   };
   return (
-    <div className="parent">
-     
-      <Slider {...settings}>
-        <div id="item1" className="carouselImg carousel-item w-full mx-auto container  ">
-          <img src="https://i.ibb.co/m026Gr4/01.png" className="w-full mx-auto" />
-          <div className="top-left  ">
-              {/* <h1 className="text-4xl text-blue-400 font-mono">Best IOT Based <br /> Air Quality <br /> Monitoring <br /> System</h1> */}
-          </div>
-        </div>
-        <div id="item2" className="carouselImg carousel-item w-full container  ">
-          <img src="https://i.ibb.co/SJXGGz7/02.png" className="w-full mx-auto" />
-        </div>
-      </Slider>
- 
+    <div className="">
+      <div class="hero py-32 bg-base-200">
+  <div class="hero-content flex-col lg:flex-row">
+    <img src="https://i.ibb.co/Zzg7PcV/banner1jpg-sizing1.jpg" class="max-w-lg p-12 rounded-lg shadow-2xl" />
+    <div className="px-12">
+      <h1 class="text-5xl font-bold text-teal-600 text-center ">Open Sky</h1>
+      <p class="py-6"> Our main product name is "Open Sky" which is Air pollution Monitoring system. It monitor the Air Quality over a webserver using internet and will trigger a alarm when the air quality goes down beyond a certain level, means when there are sufficient amount of harmful gases are present in the air like CO2, smoke, alcohol, benzene and NH3. </p>
+      <button class="btn btn-accent px-12">Details</button>
+    </div>
+  </div>
+</div>
+<div class="hero  pb-32 bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://i.ibb.co/QXNrsJ3/manage-banner.jpg" class="max-w-lg p-12 rounded-lg shadow-2xl" />
+    <div className="px-12">
+      <h1 class="text-5xl font-bold text-teal-700 text-center">Manage App</h1>
+      <p class="py-6"> Our main product name is "Open Sky" which is Air pollution Monitoring system. It monitor the Air Quality over a webserver using internet and will trigger a alarm when the air quality goes down beyond a certain level, means when there are sufficient amount of harmful gases are present in the air like CO2, smoke, alcohol, benzene and NH3. </p>
+      <button class="btn btn-accent px-12">Get Started</button>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
